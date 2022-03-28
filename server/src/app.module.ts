4b901b14f99e4ea1a8c99d013ec25ab6cd70c49d
nvminhtue <nvminhtue@gmail.com>
2022-03-28 15:57:06 +0900

    [BE] Init server base

---
+++ server/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule, WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { getConnectionOptions } from 'typeorm';
+
+import { AppConstant } from './constants/app.constant';
+import { LoggerOption } from './logger/logger.option';
+import { QueryLogger } from './logger/query.logger';
+
+const ModuleList = {
+  API: [
+    WinstonModule.forRoot(LoggerOption),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (logger) => ({
+        ...await getConnectionOptions(), logger: new QueryLogger(logger),
+      }),
+      inject: [WINSTON_MODULE_NEST_PROVIDER],
+    }),
+  ]
+}
+
+function imports() {
+  if (process.env.NODE_ENV === AppConstant.DevEnv) {
+    return ModuleList.API
+  }
+  return ModuleList.API
+}
+
+@Module({
+  imports: imports(),
+})
+export class AppModule { }
